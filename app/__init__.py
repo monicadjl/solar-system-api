0cfef61adc8b1ccb40e89508bb174e0b624452cf
Susi <susi.franco32@gmail.com>
2023-04-27 13:29:16 -0700

    added configs to connect SQLalchemy and Database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 
 def create_app(test_config=None):
     app = Flask(__name__)
-
+    
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    #connects SQL to our Database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://postgres:postgres@localhost:5432/hello_books_development'
     #add our planets blueprint
     from flask import Blueprint
     from .routes import planets_bp
